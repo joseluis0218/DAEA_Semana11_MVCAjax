b4c3e38c604e208544f4014583769c401b3beef8
Jose Luis Cristobal <jose.cristobal@techie.pe>
2019-11-12 00:57:50 -0500

    Sweet alert, reload button

--- MVCAJAX/Controllers/StudentController.cs
+++ MVCAJAX/Controllers/StudentController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using MVCAJAX.Models;
 using System.Threading.Tasks;
 
@@ -18,21 +14,6 @@ namespace MVCAJAX.Controllers
             var response = Task.Run(() => proxy.GetStudentsAsync());
             return View(response.Result.listado);
         }
-
-        //public ActionResult IndexTarea()
-        //{
-        //    var model = (from c in service.Get()
-        //                 select new StudentModel
-        //                 {
-        //                     ID = c.studentID,
-        //                     Code = c.studentCode,
-        //                     Name = c.studentName,
-        //                     LastName = c.studentLastName,
-        //                     Address = c.studentAddress,
-        //                 }).ToList();
-
-        //    return View(model);
-        //}
         public ActionResult IndexTarea()
         {
             var response = Task.Run(() => proxy.GetStudentsAsync());
